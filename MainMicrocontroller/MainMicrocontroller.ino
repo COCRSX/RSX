9058a00fe715ad4fc276c42d77f198ccedab2cb1
Luis Ivey <leivey@my.canyons.edu>
2018-12-08 18:49:52 -0800

    Interrupts + Relay

--- MainMicrocontroller/MainMicrocontroller.ino
+++ MainMicrocontroller/MainMicrocontroller.ino
@@ -1,22 +1,28 @@
-/*
+/* Programmers: 
  * Description: 
- * Interrupt pin 3
- * Interrupt pin 2
+ * 
+ * 
  * 
  */
- #include "TimerObject.h"
+ 
+#include "TimerObject.h"
 
-Timer t;
+Timer t; //New timer object for second interrupt
+volatile byte bladeState = 0; //Blades have not unlocked
+const byte activePin = 1; //Capsule MC will sense when the pin is high. 
+const byte relayPin1 = 2; //Detatch relay pin
+const byte relayPin2 = 3; //Attach battery pin
+const byte relayPin3 = 4; //Activate blade relay pin
+const int timeBlade = 10000; //Set time to unlock blades
+double temp[]; //Store data from temp sensor
+double gyro[]; //Store data from gyro sensor
+
 void setup() {
-  const byte activePin = 1; //Capsule MC will sense when the pin is high. 
-  const byte relayPin1 = 2; //Interface with detatch relay
-  const byte relayPin2 = 3; //Interface with blade relay
-  int timeBlade = 10000;
-  double temp[];
-  double gyro[];
-  attachInterrupt(digitalPinToInterrupt(activePin),detatch,HIGH);
-  t.blade();
-  blades = false;
+  pinMode(relayPin1, OUTPUT); //Initialize detach relay
+  pinMode(relayPin2, OUTPUT); //Initialize battery relay 
+  pinMode(relayPin3, OUTPUT); //Initialize blade relay
+  attachInterrupt(digitalPinToInterrupt(activePin),detatch,HIGH); //Interrupt 1 when payload MC sets pin to High
+  t.bladeUnlock(); //Interrupt 2 when timer goes off
 }
 
 void loop() {
@@ -28,24 +34,28 @@ void loop() {
 }
 
 double 3in1Temp(){
-  
+  //read in data return array
 }
 double 3in1Gyro(){
-  
+  //Read in data if altitude is a certain high call bladeUnlock() return array
 }
 double parsing(double temp, double gyro){
-  
+  //Find out how Iridium likes to recieve data return array
 }
 void iridium(double data){
-  
+  //Send data to the tranciever
 }
+
 void bladeUnlock(){
+  Serial.println("Blade Unlocking...");
+  if(bladeState == 0){ //Since we have to paths to get here we need to make sure that we dont activate the blades twice
+    digitalWrite(relayPin3, HIGH); //Open battery to solenoid 
+    bladeState = 1;
+  }
   
 }
 void detatch(){
-  
-}
-
-void blade(){
-  
+  Serial.println("Deteaching...");
+  digitalWrite(relayPin1,LOW); //Close payload MC power line
+  digitalWrite(relayPin2, HIGH); // Open on board battery line 
 }
