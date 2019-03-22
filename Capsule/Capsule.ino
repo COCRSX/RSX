d4d3a94cc3489a15d59c0478ecc78a9c5ac91cb5
Kyle Strem <kylestremdev@gmail.com>
2019-03-22 16:56:42 -0700

    Added function identifiers to all functions\nAdded capsule struct\nFinished readSignal()\nStarted on bladeUnlock()

--- Capsule/Capsule.ino
+++ Capsule/Capsule.ino
@@ -33,6 +33,9 @@ Programmers: Luis Ivey,
 // KEVIN: Figure out which pins (if any) do we need to assign for the MS5803-14BA Sensor
 #define ALTIMETERPIN 3 // MS5803_14BA
 
+// TODO: Define payload's arduino mega communication pin
+#define PAYLOADDECKMEGA 000
+
 // BME280
 Adafruit_BME280 bme; // Uses I2C protocol
 #define SEALEVELPRESSURE_HPA (1013.25)
@@ -42,15 +45,28 @@ MS5803 sfe(ADDRESS_HIGH);
 inline double altitude_ms5803(double P) return(44330.0*(1-pow(P/SEALEVELPRESSURE_HPA,1/5.255)));
 
 // Global struct for packaging transmission data
-struct TXData {
-  // TODO: Figure out what needs to get stored here
+struct capsule {
+  // BME280
+  float temperature;
+  float humidity;
+  float pressure;
+  float altitude_bme;
+
+  // SFE MS5803-14BA
+  float altitude_sfe;
+
+  // YOST 3-Space
+  float x, y, z;
+
+  // Errors
+  byte errors = 0;
 }
 
-Setup(){
+void setup(){
 	
 }
 
-loop(){
+void loop(){
 /* 1. Start reading in data	from the sensors
    2. Parse data, group, and store
    3. Iridium send data 
@@ -58,11 +74,11 @@ loop(){
 */
 }
 
-iridium (){
+void iridium (){
 	
 }
 
-bme280(){
+void bme280(){
 // Read Data (Temperature, Altitude, Pressure)
 float temperature = bme.readTemperature();
 float altitude    = bme.readAltitude(SEALEVELPRESSURE_HPA);
@@ -71,43 +87,41 @@ float pressure    = bme.readPressure() / 100.0F;
 // TODO: Update global struct with new data values
 }
 
-yostLab(){
+void yostLab(){
 // KEVIN: Write code to interface with the yostlab sensor and then update the global struct with new values.
 // Read Data
 }
 
-MS5803_14BA(){
+void MS5803_14BA(){
 // Read Data (Altitude)
 float altitude = float(altitude_ms5803(sfe.getPressure(ADC_4096)));
 
 // TODO: Update global struct with new data values
 }
 
-parsing(){
+void parsing(){
 /* Check to see if sensor is malfunctioning (Usually spits out 0 output).
 Time stamp, Organinze data, store, and make sure it is well within the data limit to send to iridium */
 	
 }
 
-bladeUnlock(){
+void bladeUnlock(){
 /* MS5803-14BA altimeter sensor on board the capsule will be our redundant blade activating system.
 If the timer is malfunctioning and has not  deployed the blades, then deploy the blades.
 If the blades have already been deployed then do nothing (Error Check 1) */	
 
 // TODO: Read altimeter sensor data from struct
-int magic_altitude;
+float altitude = capsule.altitude_bme;
 
 // TODO: If Blades have been deployed do nothing (error check?)
-if ( blades_deployed ) return 0;
-// TODO: If blades have not been deployed, deploy blades
-else {
-  // deploy blades
+if ( !blades_deployed ) {
+}
 }
 
 
 }
 
-detach(){
+void detach(){
 /*The capsule’s atmega328p microcontroller has read a high signal from the payload's Arduino Mega.
 This will cause the activation of Relay 4 to start supplying power to our capsule (inhibited batteries). 
 The signal will also cut power from Relay 3 to prevent any induced current during the action of the wire 
@@ -115,11 +129,11 @@ being pulled out from the capsule by the deck’s rod motion. */
 
 }
 
-gps(){
+void gps(){
 // Read Data	
 }
 
-timerOffRocket(){
+void timerOffRocket(){
 /*An onboard timer will start once we have received a signal from the payload's microcontroller. 
 The time interval that will take the rod to push the capsule off Wallops' rocket + leeway time, 
 will determine our timer's set time to release the capsule's blades. */
@@ -127,25 +141,26 @@ will determine our timer's set time to release the capsule's blades. */
 	
 }
 
-readSignal(){
+bool readSignal(){
 	/*The capsule’s atmega328p microcontroller has read a high signal from the payload's Arduino Mega. */
+	attachInterrupt(digitalPinToInterrupt(PAYLOADDECKMEGA), timerOffRocket, HIGH)
 }
 
-piezoOn(){
+void piezoOn(){
 	// Tone for on 
 }
 
-piezoError(){
+void piezoError(){
 	// Tone for Error
 }
 
-error4(){
+void error4(){
 	/*Capsule’s microcontroller did not read in a signal from the Arduino Mega.
 	A timer will start once the microcontroller is powered on. 
 	If the microcontroller does not receive any signal from the Arduino Mega
 	by a certain time then send an error message and cut all operations. */
 }
-error2(){
+void error2(){
 	/*Error Check 2: A accurate altimeter sensor will be used as a fail safe to eject off the rocket. 
 	If the altimeter sensor is reading an appropriate height but the timed event lines have not powered on,
 	then our payload deck’s Arduino Mega will send a signal to the capsule’s microcontroller. 
@@ -155,13 +170,13 @@ error2(){
 	We need the timed event lines to continue because one of the timed event lines is connected to our launching mechanisms. */
 }
 
-raspberryPi(){
+void raspberryPi(){
 	/* Bluetooth acting as our iridium system for testing */
 }
 
-on(){
+void on(){
 	
 }
-off(){
+void off(){
 	
 }
